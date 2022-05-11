566518c62fedfa269412c62064c6fd676c9b7b77
mbayu180 <mbayu180@gmail.com>
2022-05-11 22:54:46 +0700

    update auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Spaces()
+    {
+        return $this->hasMany(Space::class,'user_id','id');
+    }
 }

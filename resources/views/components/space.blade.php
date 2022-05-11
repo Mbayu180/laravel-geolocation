159b31965d09343a685401f9093cfe32ae2c1a8d
mbayu180 <mbayu180@gmail.com>
2022-05-11 23:25:05 +0700

    turu

---
+++ resources/views/components/space.blade.php
@@ -0,0 +1,10 @@
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <a href="#" class="btn btn-primary"> pin ! </a>
+        </div>
+        <div id="vies-space">
+            <a href="#" class="btn btn-secondary"><i class="fas fa-list"></i></a>
+            <a href="#" class="btn btn-secondary"><i class="fas fa-globe"></i></a>
+        </div>
+    </div>
